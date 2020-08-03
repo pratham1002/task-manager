90782615803b05723f67ed4452a142242f6d208a
Pratham Gupta <f20190051@pilani.bits-pilani.ac.in>
2020-08-03 22:29:33 +0530

    filtering, pagination and sorting

--- src/routers/task.js
+++ src/routers/task.js
@@ -13,12 +13,32 @@ router.post("/tasks", auth, async (req, res) => {
     }
 });
 
+// GET /tasks?completed=true
+// GET /tasks?limit=10&skip=10
+// GET /taks?sortBy=createdAt:asc
 router.get("/tasks", auth, async (req, res) => {
-    try {
-        // const tasks = await Task.find({ owner: req.user._id });
-        // res.send(tasks);
+    const match = {};
+    const sort = {};
 
-        await req.user.populate("tasks").execPopulate();
+    if (req.query.completed) {
+        match.completed = req.query.completed === "true";
+    }
+
+    if (req.query.sortBy) {
+        const parts = req.query.sortBy.split(":");
+        sort[parts[0]] = parts[1] === "desc" ? -1 : 1;
+    }
+
+    try {
+        await req.user.populate({
+            path: "tasks",
+            match,
+            options: {
+                limit: parseInt(req.query.limit),
+                skip: parseInt(req.query.skip),
+                sort
+            }
+        }).execPopulate();
         res.send(req.user.tasks);
     } catch (error) {
         res.status(400).send(error);
